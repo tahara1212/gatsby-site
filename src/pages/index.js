0205e1cfdd1a94b38ed7e6d9f9f801f50072fafa
田原隼併 <t.pe1shun@gmail.com>
2021-08-29 14:11:41 +0900

    none

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { About } from "./about"
 
 export default function Home() {
-  return <div>Hello world!2</div>
+  return (
+    <>
+      <div>Hello world!2</div>
+      <About />
+    </>
+  )
 }
